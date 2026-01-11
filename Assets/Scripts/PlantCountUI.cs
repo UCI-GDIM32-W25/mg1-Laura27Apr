047e1ea079d84ae162325a31ac1f62a1fc054deb
Laura27Apr <leyangl4@uci.edu>
2026-01-11 00:00:13 -0800

    1

--- Assets/Scripts/PlantCountUI.cs
+++ Assets/Scripts/PlantCountUI.cs
@@ -8,6 +8,7 @@ public class PlantCountUI : MonoBehaviour
 
     public void UpdateSeeds (int seedsLeft, int seedsPlanted)
     {
-        
+        _remainingText.text = "Seeds remaining: " + seedsLeft;
+        _plantedText.text = "Seeds planted: " + seedsPlanted;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,39 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        _playerTransform = transform;
+
+        _numSeedsLeft = _numSeeds;
+        _numSeedsPlanted = 0;
+
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 
     private void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(x, y, 0f);
+        _playerTransform.position += move * _speed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlantSeed();
+        }
     }
 
     public void PlantSeed ()
     {
-        
+        if (_numSeedsLeft <= 0)
+            return;
+
+        GameObject plant = Instantiate(_plantPrefab);
+        plant.transform.position = _playerTransform.position;
+
+        _numSeedsPlanted++;
+        _numSeedsLeft--;
+
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 }
